560b12c3318140630cd0b22a2e7714d2f4688db1
Taylor Dean <taylordean787@gmail.com>
2021-03-20 15:15:01 -0500

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <h1>Task Tracker</h1>
+        <Button text="Add a task" color="green"/>
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    components: {
+        Button
+    },
+    }
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+        }
+</style>
